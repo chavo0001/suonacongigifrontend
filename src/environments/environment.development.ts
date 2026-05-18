42715cd9d664add733971d25958e5bdee2143c12
gbrl-dmrz <gbrl.dmrz@gmail.com>
2026-05-18 14:36:41 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-// Riassunto: Configurazione ambientale frontend per lo sviluppo locale.
+// configurazione ambientale frontend per lo sviluppo locale
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/suonacongigi/api'
